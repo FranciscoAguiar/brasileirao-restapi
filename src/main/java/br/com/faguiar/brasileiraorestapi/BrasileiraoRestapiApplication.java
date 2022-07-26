6074a3d78817b481b499befe1167d1bdf5d0f2e7
FranciscoAguiar <fdama.cid20@uea.edu.br>
2022-07-26 16:27:28 -0400

    initial setup

---
+++ src/main/java/br/com/faguiar/brasileiraorestapi/BrasileiraoRestapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.faguiar.brasileiraorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasileiraoRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasileiraoRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/faguiar/brasileiraorestapi/BrasileiraoRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.faguiar.brasileiraorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
