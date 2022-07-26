8ded5e259968e28f2518773b0877f96717883374
FranciscoAguiar <fdama.cid20@uea.edu.br>
2022-07-26 17:43:28 -0400

    conexao jsoup com google

--- src/main/java/br/com/faguiar/brasileiraorestapi/util/ScrapingUtil.java
+++ src/main/java/br/com/faguiar/brasileiraorestapi/util/ScrapingUtil.java
@@ -1,17 +1,43 @@
 package br.com.faguiar.brasileiraorestapi.util;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.faguiar.brasileiraorestapi.dto.PartidaGoogleDTO;
 
 public class ScrapingUtil {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
 	private static final String BASE_URL_GOOGLE = "https://www.google.com/search?q=";
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		PartidaGoogleDTO partida = new PartidaGoogleDTO();
+		String url = BASE_URL_GOOGLE + "fluminense+x+bragantino+24/07/2022" + COMPLEMENTO_URL_GOOGLE;
+		
+		ScrapingUtil scraping = new ScrapingUtil();
+		scraping.obterInformacoesPartida(url);
 		
 		
 
+	}
+	
+	public PartidaGoogleDTO obterInformacoesPartida(String url) {
+		PartidaGoogleDTO partida = new PartidaGoogleDTO();
+		Document document =  null;
+		
+		try {
+			document = Jsoup.connect(url).get();
+			String title = document.title();
+			LOGGER.info("titulo da pagina: {}", title);
+		} catch (IOException e) {
+			LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE COM JSOUP : {}", e.getMessage());
+		
+		}
+		
+		return partida;
 	}
 
 }
