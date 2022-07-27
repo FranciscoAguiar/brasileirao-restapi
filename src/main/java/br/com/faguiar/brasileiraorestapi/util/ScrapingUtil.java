923dc7c0a847187dab3fd081861f7e165a757ceb
FranciscoAguiar <fdama.cid20@uea.edu.br>
2022-07-27 06:47:55 -0400

    recuperando status e tempo da partida

--- src/main/java/br/com/faguiar/brasileiraorestapi/util/ScrapingUtil.java
+++ src/main/java/br/com/faguiar/brasileiraorestapi/util/ScrapingUtil.java
@@ -15,7 +15,7 @@ public class ScrapingUtil {
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
 	
 	public static void main(String[] args) {
-		String url = BASE_URL_GOOGLE + "fluminense+x+bragantino+24/07/2022" + COMPLEMENTO_URL_GOOGLE;
+		String url = BASE_URL_GOOGLE + "fluminense+x+bragantino" + COMPLEMENTO_URL_GOOGLE;
 		
 		ScrapingUtil scraping = new ScrapingUtil();
 		scraping.obterInformacoesPartida(url);
@@ -32,12 +32,62 @@ public class ScrapingUtil {
 			document = Jsoup.connect(url).get();
 			String title = document.title();
 			LOGGER.info("titulo da pagina: {}", title);
+			StatusPartida statusPartida = obterStatusPartida(document);
+			obterTempoPartida(document);
+			
 		} catch (IOException e) {
 			LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE COM JSOUP : {}", e.getMessage());
 		
 		}
 		
 		return partida;
+	}
+	
+	public StatusPartida obterStatusPartida(Document document) {
+		
+		StatusPartida statusPartida = StatusPartida.PARTIDA_NAO_INICIADA;
+		
+		boolean isTempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").isEmpty();
+		
+		
+		
+		if (!isTempoPartida) {
+			String tempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").first().text();
+			statusPartida = StatusPartida.PARTIDA_EM_ANDAMENTO;
+			if (tempoPartida.contains("Pênaltis")) {
+				statusPartida = StatusPartida.PARTIDA_PENALTIS;
+				
+			}
+			LOGGER.info(tempoPartida);
+		}
+		
+		isTempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+		
+		if (!isTempoPartida) {
+			statusPartida = StatusPartida.PARTIDA_ENCERRADA;
+		}
+		
+		LOGGER.info("status partida : {}", statusPartida.toString());
+		
+		return statusPartida;
+				
+	}
+	
+	public String obterTempoPartida(Document document) {
+		String tempoPartida = null;
+		boolean isTempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").isEmpty();
+		if (!isTempoPartida) {
+			tempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").first().text();
+			
+		}
+		
+		isTempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+		
+		if (!isTempoPartida) {
+			tempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").first().text();
+		}
+		LOGGER.info("tempo partida : {}", tempoPartida);
+		return tempoPartida;
 	}
 
 }
